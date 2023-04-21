82b4d2a8bdc2ec0c5e94c8d3480ba875a2fe9254
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-04-21 13:57:40 +0700

    remove unnecessary commments

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,38 +1,9 @@
-// import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-// import nextPwa from "next-pwa";
+import nextPwa from "next-pwa";
 
-// const withPWA = nextPwa({
-//   dest: "public",
-//   disable: process.env.NODE_ENV === "development",
-// });
-
-// /** @type {import("next").NextConfig} */
-// const config = {
-//   reactStrictMode: true,
-//   /** Enables hot reloading for local packages without a build step */
-//   transpilePackages: ["@acme/api", "@acme/auth", "@acme/db", "@acme/ui"],
-//   /** We already do linting and typechecking as separate tasks in CI */
-//   eslint: { ignoreDuringBuilds: !!process.env.CI },
-//   typescript: { ignoreBuildErrors: !!process.env.CI },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//       },
-//     ],
-//   },
-//   webpack: (config, { isServer }) => {
-//     if (isServer) {
-//       config.plugins = [...config.plugins, new PrismaPlugin()];
-//     }
-
-//     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-//     return config;
-//   },
-// };
-
-// export default withPWA(config);
+const withPWA = nextPwa({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
@@ -44,10 +15,10 @@
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db", "@acme/ui"],
+  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
 };
 
-export default config;
+export default withPWA(config);
